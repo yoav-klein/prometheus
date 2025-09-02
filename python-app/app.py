9767d43c4f5c8f3f1510201d688b5cd88cc9dddc
yoklein <yoav.klein@cyberark.com>
2025-09-02 10:35:26 +0300

    Prometheus example

---
+++ python-app/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, Response
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+from prometheus_client import Histogram
+
+
+app = Flask(__name__)
+
+# Example metric: count HTTP requests
+REQUEST_COUNT = Counter("app_requests_total", "Total number of requests")
+HISTO = Histogram('request_latency_seconds', 'Description of histogram')
+
+
+@app.route("/")
+def index():
+    REQUEST_COUNT.inc()
+    HISTO.observe(4.7)    # Observe 4.7 (seconds in this case)
+    return "Hello, Prometheus!"
+
+
+@app.route("/metrics")
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
